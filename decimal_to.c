3e0aacebac85af38396710eedc64c4118bb68a96
Mohamed_Hamdy <120296198+Mohamed147hamdy@users.noreply.github.com>
2024-01-13 21:16:40 +0200

    first_name

---
+++ decimal_to.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void binary(int);
+void main()
+{
+	int number ;
+	printf("Enter decimal number:");
+	scanf("%d",&number);
+	printf("The  hex  number is :%x\n",number);
+	printf("The octal number is :%o\n",number);
+	printf("The binary number is :");
+	binary(number);
+}
+void binary(int n)
+{
+	if (n > 0)
+	{
+		binary(n/2);
+		printf("%d",n%2);
+	}
+}
